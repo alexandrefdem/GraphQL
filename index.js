4660e2b15061484369d635c6fd25074b564215c9
Alexandre França de Magalhães <alexandredemagalhaess@gmail.com>
2021-12-04 09:37:26 -0300

    Add basic book query

---
+++ index.js
@@ -0,0 +1,76 @@
+const { response } = require('express');
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql');
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLList,
+    GraphQLInt,
+    GraphQLNonNull
+} = require('graphql')
+const app = express()
+
+
+const authors = [
+	{ id: 1, name: 'J. K. Rowling' },
+	{ id: 2, name: 'J. R. R. Tolkien' },
+	{ id: 3, name: 'Brent Weeks' }
+]
+
+const books = [
+	{ id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
+	{ id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
+	{ id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1 },
+	{ id: 4, name: 'The Fellowship of the Ring', authorId: 2 },
+	{ id: 5, name: 'The Two Towers', authorId: 2 },
+	{ id: 6, name: 'The Return of the King', authorId: 2 },
+	{ id: 7, name: 'The Way of Shadows', authorId: 3 },
+	{ id: 8, name: 'Beyond the Shadows', authorId: 3 }
+]
+
+/*const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'helloworld',
+        fields: () => ({
+            message: {
+                 type: GraphQLString,
+                 resolve: () => 'Hello World'
+                }
+        })
+    })
+})
+*/
+
+const BookType = new GraphQLObjectType({
+    name: 'book',
+    description: 'This is a book',
+    fields: () => ({
+        id: { type: new GraphQLNonNull(GraphQLInt), },
+        name: { type: new GraphQLNonNull(GraphQLString)},
+        authorId: { type: new GraphQLNonNull(GraphQLInt)}
+    })
+})
+
+const RootQueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: 'Root Query',
+    fields: () => ({
+        books: {
+            type: new GraphQLList(BookType),
+            description: 'List of Books',
+            resolve: () => books
+        }
+    })
+})
+
+
+const schema = new GraphQLSchema({
+    query: RootQueryType
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}))
+app.listen(5000., () => console.log('server running'))
